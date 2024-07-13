bc891bd5bd7ce6702645307cb0a923bb636497de
Manolis <loloman73@yahoo.com>
2024-07-13 20:16:57 +0300

    Thymeleaf simple dynamic web page OK

--- web-adapter/src/main/kotlin/xenagos/adapter/web/LoadLightXensForAreaController.kt
+++ web-adapter/src/main/kotlin/xenagos/adapter/web/LoadLightXensForAreaController.kt
@@ -2,30 +2,33 @@ package xenagos.adapter.web
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RestController
+import xenagos.application.domain.xenagisi.entity.XenUUID
 import xenagos.application.port.commun.model.LightXenDTO
 import xenagos.application.port.input.GetLightXensForAreaInPort
 
 @Controller
 class LoadLightXensForAreaController( @Autowired private val loadLightXensForAreaService: GetLightXensForAreaInPort) {
 
-    @GetMapping("/testStatic")
-    fun loadPage():String {
-        return "redirect:/test.html"
-    }
 
-    @ResponseBody
-    @GetMapping("/area/{area}/", produces = ["application/json"])
-    fun testReturnJSON(@PathVariable("area") area: Int): List<LightXenDTO>? {
+    @GetMapping("/{area}")
+    fun testThyme(@PathVariable("area") area: Int, model: Model): String{
 
         val lightXensForArea: List<LightXenDTO>? = loadLightXensForAreaService.whereAreaIs(area)
 
-        return lightXensForArea
-    }
+        var xenIdList = mutableListOf<XenUUID>()
+        lightXensForArea?.forEach {xenIdList.add(it.xenId)  }
 
+
+        model.addAttribute("area", area)
+        model.addAttribute("XenID", xenIdList)
+        model.addAttribute("Xens", lightXensForArea)
+
+        return "index"
+    }
 
 
 }

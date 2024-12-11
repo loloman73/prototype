5582a2ea5fcc865bee9051e146f95eed2a303a07
Manolis <loloman73@yahoo.com>
2024-12-11 22:16:31 +0200

    with FlyWay

--- persistence-adapter/build.gradle.kts
+++ persistence-adapter/build.gradle.kts
@@ -6,5 +6,6 @@ dependencies {
     implementation(project(":application"))
     implementation(libs.sb.starter)
     implementation(libs.sb.starter.data.jpa)
+    implementation(libs.flywayPostgresql)
     runtimeOnly(libs.postgres)
 }

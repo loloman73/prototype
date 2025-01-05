56ef5ad90f7c479836198708bf513dd9b89d4173
manolis <manolis@officePC>
2025-01-05 19:09:34 +0200

    admin page (working on UI of left buttons) - nested layouts

--- web-adapter/src/main/kotlin/xenagos/adapter/web/AdminPageController.kt
+++ web-adapter/src/main/kotlin/xenagos/adapter/web/AdminPageController.kt
@@ -10,9 +10,43 @@ import org.springframework.web.bind.annotation.RequestMapping
 class AdminPageAdapter {
 
     @GetMapping
-    fun adminPage():String{
+    fun mainAdminPage():String{
+        return "adminMain"
+    }
 
-        return "admin"
+    @GetMapping("/AccessibilityTags/")
+    fun accessibilityTags():String{
+        return "adminAccessibilityTags"
+    }
+
+    @GetMapping("/TopicTags/")
+    fun topicTags():String{
+        return "adminTopicTags"
+    }
+
+    @GetMapping("/AgeRanges/")
+    fun ageRanges():String{
+        return "adminAgeRanges"
+    }
+
+    @GetMapping("/Languages/")
+    fun languages():String{
+        return "adminLanguages"
+    }
+
+    @GetMapping("/MediaTypes/")
+    fun mediaTypes():String{
+        return "adminMediaTypes"
+    }
+
+    @GetMapping("/DurationTypes/")
+    fun durationTypes():String{
+        return "adminDurationTypes"
+    }
+
+    @GetMapping("/AmeAFriendlyTypes/")
+    fun ameAFriendlyTypes():String{
+        return "adminAmeAFriendlyTypes"
     }
 
 }

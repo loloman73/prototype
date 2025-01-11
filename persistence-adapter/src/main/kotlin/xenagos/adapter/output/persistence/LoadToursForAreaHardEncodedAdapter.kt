70313c1998d531a975f4afcf4167838fd7fa222e
Manolis <loloman73@yahoo.com>
2025-01-11 22:30:57 +0200

    1. add livereload.js to html 2. add cache=false to application.properties

--- persistence-adapter/src/main/kotlin/xenagos/adapter/output/persistence/LoadToursForAreaHardEncodedAdapter.kt
+++ persistence-adapter/src/main/kotlin/xenagos/adapter/output/persistence/LoadToursForAreaHardEncodedAdapter.kt
@@ -6,42 +6,27 @@ import xenagos.domain.model.TourUUID
 import xenagos.commun.model.Money
 import xenagos.application.port.output.LoadToursForAreaOutPort
 import java.util.*
+import kotlin.random.Random
 
 @Service
 class LoadToursForAreaHardEncodedAdapter : LoadToursForAreaOutPort {
     override fun whereAreaIs(area: Int): Set<Tour>? {
-        if (area == 51) {
-            val x1 = Tour(
-                tourId = TourUUID(UUID.randomUUID()),
-                title = "",
-                description = "",
-                photoFileName = "",
-                totalRate = 0,
-                totalReviews = 0,
-                price = Money(12, Currency.getInstance("EUR")),
-            )
-
-            val x2 = Tour(
-                tourId = TourUUID(UUID.randomUUID()),
-                title = "",
-                description = "",
-                photoFileName = "",
-                totalRate = 0,
-                totalReviews = 0,
-                price = Money(23, Currency.getInstance("EUR")),
-            )
-
-            val x3 = Tour(
-                tourId = TourUUID(UUID.randomUUID()),
-                title = "",
-                description = "",
-                photoFileName = "",
-                totalRate = 0,
-                totalReviews = 0,
-                price = Money(49, Currency.getInstance("EUR")),
-            )
-
-            return setOf(x1, x2, x3)
-        } else return null
+        return if (area == 51) {
+            setOf(fakeTour(), fakeTour(), fakeTour(), fakeTour(), fakeTour(), fakeTour(),fakeTour(),fakeTour())
+           // setOf(fakeTour(), fakeTour())
+        } else null
     }
+
+    private fun fakeTour(): Tour {
+        return Tour(
+            tourId = TourUUID(UUID.randomUUID()),
+            title = "",
+            description = "",
+            photoFileName = "",
+            totalRate = Random.nextInt(5).toByte(),
+            totalReviews = Random.nextInt(100),
+            price = Money(Random.nextInt(10), Currency.getInstance("EUR")),
+        )
+    }
+
 }

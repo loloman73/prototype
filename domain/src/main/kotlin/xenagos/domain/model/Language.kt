56e73137dd5880551aff6a4ddefe600f62cd59b3
Manolis <loloman73@yahoo.com>
2025-04-07 21:41:27 +0200

    star Admin / Languages CRUD

--- domain/src/main/kotlin/xenagos/domain/model/Language.kt
+++ domain/src/main/kotlin/xenagos/domain/model/Language.kt
@@ -2,15 +2,19 @@ package xenagos.domain.model
 
 import java.util.*
 
-data class Language(val id: UUID,
-                    val code: String,
-                    val englishName:String,
-                    val localName:String)
+data class Language(
+    val id: UUID,
+    val code: String,
+    val englishName: String,
+    val nativeName: String
+)
 
-//en("English","English"),
-//de("German","Deutsch"),
-//fr("French","Français"),
-//ru("Russian","Русский"),
-//es("Spanish","Español"),
-//zh("Chinese","中文"),
-//tr("Turkish","Türkçe")
+//code: ISO 639-3
+//eng, "English","English"
+//ell, "Greek", "Ελληνικά"
+//deu, "German","Deutsch"
+//fra, "French","Français"
+//ru, "Russian","Русский"
+//es, "Spanish","Español"
+//zh, "Chinese","中文"
+//tur, "Turkish","Türkçe"

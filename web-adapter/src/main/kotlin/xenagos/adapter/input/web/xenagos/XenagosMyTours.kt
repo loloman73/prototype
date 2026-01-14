a6f0c59963c182d797c9dbe37c4b98fd52c874ef
manolis <manolis@officePC>
2026-01-14 16:30:31 +0200

    fix IDE code error when thymeleaf layout has parameters

--- web-adapter/src/main/kotlin/xenagos/adapter/input/web/xenagos/XenagosMyTours.kt
+++ web-adapter/src/main/kotlin/xenagos/adapter/input/web/xenagos/XenagosMyTours.kt
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import kotlin.collections.listOf
 
 class Tour(val id: String, val description: String, val region: String, val status: String)
+class TourPublished(val id: String, val description: String, val region: String, publishedDate: String)
 
 @Controller
 @RequestMapping("/xenagos/my-tours")
@@ -15,38 +16,38 @@ class XenagosMyTours {
 
     fun myToursAll(): List<Tour> {
         val tour1 = Tour("T-11111", "Tour 1 description", "Greece", "Published")
-        val tour2 = Tour("T-22222", "Tour 2 description", "Egypt", "Unpublished")
-        val tour3 = Tour("T-33333", "Tour 3 description", "Peru", "Deactivated")
-        return  listOf(tour1, tour2, tour3)
+        val tour2 = Tour("T-22222", "Tour 2 description", "Egypt", "Published")
+        val tour3 = Tour("T-2a2a2a2a2a", "Tour 2a description", "Peru", "Unpublished")
+        val tour4 = Tour("T-3a3a3a3a3a", "Tour 3a description", "Greece", "Unpublished")
+        val tour5 = Tour("T-3b3b3b3b", "Tour 3b description", "Egypt", "Deactivated")
+        return  listOf(tour1, tour2, tour3, tour4, tour5)
     }
-    fun myToursUnpublished(): List<Tour> {
-        val tour2 = Tour("T-22222", "Tour 2 description", "Egypt", "Unpublished")
-        return  listOf(tour2)
+    fun myToursPublished(): List<TourPublished> {
+        val tour1 = TourPublished("T-11-pub", "Tour 1 description", "Greece", "13/01/2024")
+        val tour2 = TourPublished("T-22-pub", "Tour 2 description", "Greece", "23/01/2025")
+        return  listOf(tour1, tour2)
     }
-    fun myToursDeactivated(): List<Tour> {
-        val tour3 = Tour("T-33333", "Tour 3 description", "Peru", "Deactivated")
-        return  listOf(tour3)
-    }
-
 
 
     @GetMapping
     fun showMyTours(model: Model, @RequestParam status: String?): String {
 
-        if (status == "published") {
+        if (status == "all") {
             model.addAttribute("myToursAll", myToursAll())
             return "pages/xenagos/xenagosMyToursAll"
         }
+        if (status == "published") {
+            model.addAttribute("myToursPublished", myToursPublished())
+            return "pages/xenagos/xenagosMyToursPublished"
+        }
         if (status == "unpublished") {
-            model.addAttribute("myToursUnpublilshed", myToursUnpublished())
             return "pages/xenagos/xenagosMyToursUnpublished"
         }
         if (status == "deactivated") {
-            model.addAttribute("myToursdeactivated", myToursDeactivated())
             return "pages/xenagos/xenagosMyToursDeactivated"
         }
 
-        return "redirect:/xenagos/my-tours?status=published"
+        return "redirect:/xenagos/my-tours?status=all"
     }
 
 }
